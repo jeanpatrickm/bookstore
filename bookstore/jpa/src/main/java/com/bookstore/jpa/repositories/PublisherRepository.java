b276b70a6030eee5033509d9fb608e3f15ba7b2f
Jean Patrick <100332558+jeanpatrickm@users.noreply.github.com>
2024-11-05 23:20:45 -0300

    Delete bookstore/jpa directory

--- bookstore/jpa/src/main/java/com/bookstore/jpa/repositories/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.jpa.repositories;
-
-import com.bookstore.jpa.models.PublisherModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PublisherRepository extends JpaRepository<PublisherModel, UUID> {
-}
